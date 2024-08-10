cde88f2fc41601261a9886fcc95a85e7857da8c7
Sergei Zavgorodnii <zavgorodny@uchi.ru>
2024-08-10 15:57:03 +0300

    separate MockCache from server code; refactoring

---
+++ src/mock-cache.ts
@@ -0,0 +1,71 @@
+import type {
+  MockAction,
+  MockRouter,
+  RouteAction,
+  RouterExtended,
+} from './types';
+
+const PARAM_RE = /:(\w+)/g;
+
+export class MockCache {
+  routes = {} as Record<string, RouterExtended>;
+
+  constructor(router: MockRouter) {
+    this.init(router);
+  }
+
+  push(key: string, action: MockAction): RouterExtended {
+    const params: string[] = [];
+
+    const pattern = key.replace(PARAM_RE, function (_, key) {
+      params.push(key);
+
+      return '([^\\/]+)';
+    });
+
+    this.routes[key] = {
+      pattern: new RegExp(`^${pattern}(?:\\?.+)?$`),
+      params,
+      action,
+    };
+
+    return this.routes[key];
+  }
+
+  get(key: string): RouterExtended | null {
+    return this.routes[key] || null;
+  }
+
+  init(router: MockRouter) {
+    Object.keys(router).forEach((key) => {
+      this.push(key, router[key]);
+    });
+
+    return this;
+  }
+
+  iterate(callback: (route: RouterExtended) => void) {
+    Object.keys(this.routes).forEach((key) => callback(this.routes[key]));
+  }
+
+  search(url: string): RouteAction | null {
+    for (const key in this.routes) {
+      const { pattern, params, action } = this.routes[key];
+      const match = pattern.exec(url);
+
+      if (match) {
+        const paramsMapped = params.reduce<Record<string, string>>(
+          (result, key, index) => {
+            result[key] = match[index + 1];
+            return result;
+          },
+          {},
+        );
+
+        return { action: action, params: paramsMapped, key };
+      }
+    }
+
+    return null;
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import http from 'http';
+import { MockCache } from './mock-cache';
 import type {
   ClientRequest,
   MockAction,
@@ -10,79 +11,21 @@ import type {
 } from './types';
 import { respond } from './utils';
 
-const SEARCH_QUERY_RE = /\?.+$/;
-const PARAM_RE = /:(\w+)/g;
-
 const SERVER_RESTART_DELAY = 200;
+
+const log = (message: string) => console.log(`[MOCK] ${message}`);
 
 export const useMockServer = (
   config: MockRouter,
   params: MockServerParams = {},
 ) => {
-  const mockRegExpCache = {
-    routes: {} as Record<string, RouterExtended>,
-
-    push(key: string, action: MockAction): RouterExtended {
-      const params: string[] = [];
-
-      const pattern = key.replace(PARAM_RE, function (_, key) {
-        params.push(key);
-
-        return '([^\\/]+)';
-      });
-
-      this.routes[key] = {
-        pattern: new RegExp(`^${pattern}([?]([\\[\\]\\w%=]+&?)+)?$`),
-        params,
-        action,
-      };
-
-      return this.routes[key];
-    },
-
-    get(key: string): RouterExtended | null {
-      return this.routes[key] || null;
-    },
-
-    init(router: MockRouter) {
-      Object.keys(router).forEach((key) => {
-        this.push(key, router[key]);
-      });
-
-      return this;
-    },
-
-    iterate(callback: (route: RouterExtended) => void) {
-      Object.keys(this.routes).forEach((key) => callback(this.routes[key]));
-    },
-
-    search(url: string): RouteAction | null {
-      for (const key in this.routes) {
-        const { pattern, params, action } = this.routes[key];
-        const match = pattern.exec(url);
-
-        if (match) {
-          const paramsMapped = params.reduce<Record<string, string>>(
-            (result, key, index) => {
-              result[key] = match[index + 1];
-              return result;
-            },
-            {},
-          );
-
-          return { action: action, params: paramsMapped, key };
-        }
-      }
-
-      return null;
-    },
-  }.init(config);
+  const mockRegExpCache = new MockCache(config);
 
   const getRequestMockFunction = (
     request: ClientRequest,
     response: ServerResponse,
   ) => {
-    const url = request.url?.replace(SEARCH_QUERY_RE, '');
+    const url = request.url;
     const mock = url ? mockRegExpCache.search(url) : null;
 
     if (mock) {
@@ -142,7 +85,7 @@ export const useMockServer = (
     response: ServerResponse,
   ) {
     if (TerminationRequest.reply(request, response)) {
-      console.log('Mock server is being closed');
+      log('Mock server is being closed');
       this.close();
       return;
     }
@@ -164,7 +107,7 @@ export const useMockServer = (
         })
         .catch((error) => {
           if (error instanceof Error) {
-            console.log('Error at route:', mocked.mock.key);
+            log(`Error at route: ${mocked.mock.key}`);
             console.log(error);
             return;
           }
@@ -186,19 +129,19 @@ export const useMockServer = (
               setTimeout(() => startServer(port), SERVER_RESTART_DELAY);
             })
             .catch(() => {
-              console.log(`Failed to start Mock server. Port ${port} is busy.`);
+              log(`Failed to start Mock server. Port ${port} is busy.`);
             });
         } else {
           throw error;
         }
       })
       .listen(port, () =>
-        console.log(`Mock server is started on port ${port}`),
+        log(`Mock server is started on port ${port}`),
       );
   };
 
   const mockServerPlugin = () => ({
-    name: 'start-mock-server',
+    name: 'start-mockerver',
     configureServer() {
       if (params.port) {
         startServer(params.port);
